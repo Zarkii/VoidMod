4528406ee29eafb3c7bbcf18cb877d8bfe5b3b4c
Zarkii <35203783+Zarkii@users.noreply.github.com>
2018-11-01 18:41:24 +0100

    Initial commit

---
+++ src/main/java/com/kocininja/voidmod/Main.java
@@ -0,0 +1,8 @@
+package com.kocininja.voidmod;
+
+import com.kocininja.voidmod.util.Reference;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Reference.MOD_ID,name = Reference.NAME,version = Reference.VERSION)
+public class Main {
+}
---
+++ src/main/java/com/kocininja/voidmod/util/Reference.java
@@ -0,0 +1,11 @@
+package com.kocininja.voidmod.util;
+
+public class Reference {
+    public static final String MOD_ID = "vm";
+    public static final String NAME = "Void Mod";
+    public static final String VERSION = "0.1";
+    public static final String ACCEPTED_VERSIONS = "[1.12.2]";
+    public static final String CLIENT_PROXY_CLASS = "";
+    public static final String COMMON_PROXY_CLASS = "";
+
+}
